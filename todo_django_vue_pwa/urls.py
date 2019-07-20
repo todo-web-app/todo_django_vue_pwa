253095b475aba58c7c001aba7c46730b25140969
Bryan Calupitan <bryan@luxeys.com>
2019-07-21 01:02:36 +0900

    Serve vue app in django

--- todo_django_vue_pwa/urls.py
+++ todo_django_vue_pwa/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('todo/', include('todo.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('', TemplateView.as_view(template_name='index.html')),
 ]
