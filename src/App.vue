3876394fb539ecf4281a725146530d34d42e35da
Bryan Calupitan <bryan@luxeys.com>
2019-07-21 01:44:31 +0900

    Vue app call django rest api with axis

--- src/App.vue
+++ src/App.vue
@@ -1,59 +1,31 @@
 <template>
   <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
-    </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <hello></hello>
-    </main>
+    <p>
+      {{lists}}
+    </p>
   </div>
 </template>
-
 <script>
-import Hello from './components/Hello'
-
+import axios from 'axios';
 export default {
-  name: 'app',
-  components: {
-    Hello
+  name: 'App',
+  data() {
+      return {
+          lists: null,
+      }
+  },
+  mounted: function() {
+    this.FetchData();
+  },
+  methods: {
+      FetchData: function() {
+          var app = this;
+          axios.get("/todo/list").then(response => {
+              app.lists = response.data;
+          });
+      },
   }
 }
 </script>
-
 <style>
-body {
-  margin: 0;
-}
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-main {
-  text-align: center;
-  margin-top: 40px;
-}
-
-header {
-  margin: 0;
-  height: 56px;
-  padding: 0 16px 0 24px;
-  background-color: #35495E;
-  color: #ffffff;
-}
-
-header span {
-  display: block;
-  position: relative;
-  font-size: 20px;
-  line-height: 1;
-  letter-spacing: .02em;
-  font-weight: 400;
-  box-sizing: border-box;
-  padding-top: 16px;
-}
 </style>
