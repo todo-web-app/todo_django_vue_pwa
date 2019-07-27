9b6d8702f3899c2b51fc453ccb2b8f1e0cbb7072
Bryan Calupitan <bryan@luxeys.com>
2019-07-27 13:49:27 +0900

    Date fix

--- todo/models.py
+++ todo/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class List(models.Model):
     title = models.CharField(max_length=100)
     done = models.BooleanField(default=False)
-    created = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         ordering = ('created',)
