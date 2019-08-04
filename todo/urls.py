6d813d1b8b38206177bf2b7a1fc0fc3059b618f8
Bryan Calupitan <bryan@luxeys.com>
2019-08-05 00:14:01 +0900

    delete route csrf ignore

--- todo/urls.py
+++ todo/urls.py
@@ -7,5 +7,6 @@ router = routers.DefaultRouter()
 router.register(r'list', views.ListViewSet)
 
 urlpatterns = [
+    path('list/<int:pk>', views.ListViewSet.as_view({'delete': 'destroy'})),
     path('', include(router.urls)),
 ]
