2a6568e947ae81657af84b12ccc229969e1f7edd
Bryan Calupitan <bryan@luxeys.com>
2019-08-14 15:03:53 +0900

    Home design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,16 +1,17 @@
 <template>
-    <div>
-        <h1>Central list for your family to share...</h1>
-        <b-button type="submit" variant="primary" :to="{ name: 'login' }">Create your list now!</b-button>
-    </div>
+  <div>
+    <b-jumbotron header="Family List" lead="Central list for your family to share">
+      <b-button type="submit" variant="primary" :to="{ name: 'login' }">Create your list now!</b-button>
+    </b-jumbotron>
+  </div>
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            'home': 'home'
-        }
-    }
-}
+  data() {
+    return {
+      home: "home"
+    };
+  }
+};
 </script>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,12 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="success">
       <b-container>
-        <b-navbar-brand :to="{ name: 'home' }">
+        
+        <b-navbar-brand :to="{ name: 'home' }" type="dark">
+          <i class="material-icons secondary float-left">accessibility</i>
           Family List
         </b-navbar-brand>
+        
         <b-navbar-nav class="ml-auto" v-if="show">
           <b-nav-item href="#" @click.prevent="logout">Sign Out</b-nav-item>
         </b-navbar-nav>
