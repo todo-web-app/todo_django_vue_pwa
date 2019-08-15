677c69f937542d6883d22aa162cc1de3f6e98299
Bryan Calupitan <bryan.calupitan@gmail.com>
2019-08-15 14:57:22 +0900

    Nav changes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -7,12 +7,14 @@
           Family List
         </b-navbar-brand>
 
-        <b-navbar-toggle target="nav-collapse" v-if="logged_in"></b-navbar-toggle>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-        <b-collapse id="nav-collapse" is-nav v-if="logged_in"> 
+        <b-collapse id="nav-collapse" is-nav> 
           <b-navbar-nav class="ml-auto">
-            <b-nav-item :to="{ name: 'list' }">My List</b-nav-item>
-            <b-nav-item href="#" @click.prevent="logout">Sign Out</b-nav-item>
+            <b-nav-item :to="{ name: 'login' }" v-show="!logged_in">Sign In</b-nav-item>
+            <b-nav-item :to="{ name: 'register' }" v-show="!logged_in">Register</b-nav-item>
+            <b-nav-item :to="{ name: 'list' }" v-show="logged_in">My List</b-nav-item>
+            <b-nav-item href="#" @click.prevent="logout" v-show="logged_in">Sign Out</b-nav-item>
           </b-navbar-nav>
         </b-collapse>
       </b-container>
