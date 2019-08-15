45c3b6a9263c3ba176883ef53c3592f1171b21ef
Bryan Calupitan <bryan@luxeys.com>
2019-08-15 14:39:46 +0900

    Nav change

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,15 +2,19 @@
   <div>
     <b-navbar toggleable="lg" type="dark" variant="success">
       <b-container>
-        
         <b-navbar-brand :to="{ name: 'home' }" type="dark">
           <i class="material-icons secondary float-left">accessibility</i>
           Family List
         </b-navbar-brand>
-        
-        <b-navbar-nav class="ml-auto" v-if="logged_in">
-          <b-nav-item href="#" @click.prevent="logout">Sign Out</b-nav-item>
-        </b-navbar-nav>
+
+        <b-navbar-toggle target="nav-collapse" v-if="logged_in"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav v-if="logged_in"> 
+          <b-navbar-nav class="ml-auto">
+            <b-nav-item :to="{ name: 'list' }">My List</b-nav-item>
+            <b-nav-item href="#" @click.prevent="logout">Sign Out</b-nav-item>
+          </b-navbar-nav>
+        </b-collapse>
       </b-container>
     </b-navbar>
   </div>
